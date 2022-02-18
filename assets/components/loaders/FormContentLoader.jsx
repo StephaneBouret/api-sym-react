ecfb35d9c4818e45448d7895c2da28ad80b3d33c
Stéphane BOURET <sbouret@orange.fr>
2022-02-18 11:50:29 +0100

    Centralisation de la configuration des adresses API

--- assets/components/loaders/FormContentLoader.jsx
+++ assets/components/loaders/FormContentLoader.jsx
@@ -6,8 +6,8 @@ const FormContentLoader = props => (
     height={500}
     width={800}
     speed={2}
-    primaryColor="#f3f3f3"
-    secondaryColor="#ecebeb"
+    primarycolor="#f3f3f3"
+    secondarycolor="#ecebeb"
     {...props}
   >
     <rect x="0" y="10" rx="0" ry="0" width="151" height="15" />
--- assets/services/usersAPI.js
+++ assets/services/usersAPI.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { USERS_API } from "../config";
 
 function register(user) {
     return axios.post(
-        "https://127.0.0.1:8000/api/users", user);
+        USERS_API, user);
 }
 
 export default {
